b7d767d12894ab281cba2d96a9d6bf7fc5d23ecd
MatBustamant <matexequiel.2012@gmail.com>
2026-06-28 14:22:57 -0300

    Agregar resto de archivos del backend

---
+++ backend/src/main/java/com/grupocapa8/persis/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.grupocapa8.persis;
+
+/**
+ *
+ * @author Matias
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
